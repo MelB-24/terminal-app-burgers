30baf1f2e0042c90daa83503f4c8be9c7dab0df0
Melissa Bykersma <melissabykersma@gmail.com>
2019-09-06 11:24:00 +1000

    updated

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 # allows you to execute ./build.sh without the bash keyword
 chmod +x ./build.sh
 # copy across files to distribution directory
-mkdir burger-terminal-app
-cp ./burger.rb ./burger-terminal-app
-cp ./ascii.rb ./burger-terminal-app
+mkdir dist
+cp ./src./burger.rb ./dist
+cp ./ascii.rb ./dist
 # install all gems
 gem install colorize 
 gem install tty-prompt
